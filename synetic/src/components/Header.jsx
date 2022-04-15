5f0cb8db135b703a19312dd1e2c3bfcbede250b1
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-15 19:54:45 +0100

    h1 alignment

--- synetic/src/components/Header.jsx
+++ synetic/src/components/Header.jsx
@@ -5,12 +5,13 @@ export default function Header(){
         <div id="main">
           
             <div className="header-heading">
-               <h2>Get your </h2>
-             <h1>FITNESS<span> GAME ON </span>WITH US</h1>
-             <p className="details">Build your best self with professional fitness experts </p>
-             <p className="details"> and cutting edge equipment</p>
+               <h2>Lorem  </h2>
+             <h1>ipsum<span> dolor sit</span>amet.</h1>
+              
+             <p className="details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, saepe?</p>
+             <p className="details"> Lorem ipsum dolor sit amet.</p>
              <div className="header-btn">
-                 <a href="mailto:vaishalimehmi@yahoo.co.uk" className="header-btn">JOIN US TODAY</a>
+                 <a href="mailto:vaishalimehmi@yahoo.co.uk" className="header-btn">Lorem, ipsum dolor.</a>
              </div>
                 </div></div>
       

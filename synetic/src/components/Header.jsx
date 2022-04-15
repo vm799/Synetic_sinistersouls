4431f25cc4398645085774e3aa30f36a34132e3b
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-15 20:03:16 +0100

    Header component

--- synetic/src/components/Header.jsx
+++ synetic/src/components/Header.jsx
@@ -5,8 +5,8 @@ export default function Header(){
         <div id="main">
           
             <div className="header-heading">
-               <h2>Lorem  </h2>
-             <h1>ipsum<span> dolor sit</span>amet.</h1>
+               <h2>NFT GAMING</h2>
+             <h1>SYNETIC<span> sinister</span>souls</h1>
               
              <p className="details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, saepe?</p>
              <p className="details"> Lorem ipsum dolor sit amet.</p>

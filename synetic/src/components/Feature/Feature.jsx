fa7ab0a2012a8db7511f8d737fdb7b5616dbe618
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-16 15:35:14 +0100

    image files

--- synetic/src/components/Feature/Feature.jsx
+++ synetic/src/components/Feature/Feature.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import FeatureBox from "../FeatureBox/FeatureBox"
-import fimage1 from "../images/1.png"
-import fimage2 from "../images/2.png"
-import fimage3 from "../images/3.png"
-import fimage4 from "../images/4.png"
+// import fimage1 from ".../images/1.png"
+import fimage1 from 
+"https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
+import fimage2 from 
+"https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
+import fimage3 from 
+"https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
+
 
 export default function Feature(){
     return(
         <div id="features">
             <h1>Features</h1>
             <div className="feature-container">
-                <FeatureBox image={fimage1} title="Strength training" />
-                <FeatureBox image={fimage2} title="Nutrition" />
-                <FeatureBox image={fimage3} title="Wellbeing" />
-                <FeatureBox image={fimage4} title="Flexibility" />
+                <FeatureBox image={fimage1} title="NFTS" />
+                <FeatureBox image={fimage2} title="GAMING" />
+                <FeatureBox image={fimage3} title="SYNEVERSE" />
+                {/* <FeatureBox image={fimage4} title="Flexibility" /> */}
 
                 </div>
         </div>
--- synetic/src/components/Navbar/Navbar.jsx
+++ synetic/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../images/logo.png"
+
 import {Link} from 'react-scroll'
 
 export default function Navbar(){

6908192d599e0b2466a2ab43c8ffdd19b45f2327
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-16 17:27:47 +0100

    centering hero

--- synetic/src/components/Hero/Hero.jsx
+++ synetic/src/components/Hero/Hero.jsx
@@ -3,15 +3,15 @@ import "../Hero/Hero.css"
 
 export default function Hero(){
     return(
-        <div className="hero">
+        <div className="hero section-transition">
             <div className="hero-text">
                 <h1>SYNETIC</h1>
                 <p>Welcome to the SINEVERSE</p>
            
-            <button>CONNECT WALLET</button>
+            <button className="hero-btn">CONNECT WALLET</button>
             </div>
 
-            <div id="hero-image">
+            <div className="hero-image">
             <img src="https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60" alt="" />
             </div>
         </div>

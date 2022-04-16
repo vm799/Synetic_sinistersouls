fea7fc408ef73ffbb30aa91e0750bc2ce53a13de
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-16 16:19:36 +0100

    font sizes hero

--- synetic/src/components/Hero/Hero.jsx
+++ synetic/src/components/Hero/Hero.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import "../Hero/Hero.css"
 
 export default function Hero(){
     return(
-        <div id="about">
-        <div id="about-image">
-            <img src="https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60" alt="" /></div>
-            <div className="about-text">
-                <h1>Learn more about who we are</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta aliquam pariatur ut assumenda, ipsum in, alias esse amet molestiae a incidunt et dolor culpa quidem, doloremque tenetur dolorum modi. Debitis.</p>
+        <div className="hero">
+            <div className="hero-text">
+                <h1>SYNETIC</h1>
+                <p>Welcome to the SINEVERSE</p>
            
-            <button>READ MORE</button>
+            <button>CONNECT WALLET</button>
+            </div>
+
+            <div id="hero-image">
+            <img src="https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60" alt="" />
             </div>
         </div>
       

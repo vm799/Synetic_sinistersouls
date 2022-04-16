14c42c5b3f5722f02bcbad52d83862faa04241c4
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-16 14:52:56 +0100

    restructuring files

--- synetic/src/App.js
+++ synetic/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import Navbar from "./Components/Navbar"
-import Feature from "./Components/Feature"
+import Navbar from "./Components/Navbar/Navbar"
+import Feature from "./Components/Feature/Feature"
 import Offer from "./Components/Offer"
-import About from "./Components/About"
-import Header from "./Components/Header";
-import Contact from "./Components/Contact";
-import Footer from "./Components/Footer";
+import About from "./Components/About/About"
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <Hero />
      <Header />
      <Feature />
      <Offer />

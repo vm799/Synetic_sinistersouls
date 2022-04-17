e64d9334c2f66351e5c296cdd9736159a64928ca
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-17 08:32:29 +0100

    syneverse invitation block

--- synetic/src/components/Offer.jsx
+++ synetic/src/components/Offer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Offer(){
     return(
         <div id="offer">
-            <div className="pr-heading"><h1> A BIG <span>OFFER </span>FOR 2022</h1>
+            <div className="pr-heading"><h1> Step into the<span>SYNEVERSE </span></h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum hic nam atque maiores modi fugiat.</p>
                 <div className="pr-btns">
-                    <a href="#" className="pr-btn">JOIN NOW</a>
+                    <a href="#" className="pr-btn">ENTER HERE</a>
                 </div>
            
             </div>

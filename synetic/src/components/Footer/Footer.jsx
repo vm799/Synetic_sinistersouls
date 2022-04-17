6f2f6b18bc1240bb7939766eea8497ae618deb8f
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-17 21:59:34 +0100

    about block

--- synetic/src/components/Footer/Footer.jsx
+++ synetic/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
   
     return (
     <footer class="footer">
-        <a href="mailto:vaishalimehmi@yahoo.co.uk" class="footer__link">vaishalimehmi@yahoo.co.uk</a>
+        <a href="sinistersouls2922@gmail.com" class="footer__link">sinistersouls2922@gmail.com</a>
         <ul class="social-list">
             <li class="social-list__item">
                 <a class="social-list__link" href="https://uxfolio.com">
@@ -19,12 +19,7 @@ export default function Footer(){
                             <i class="fab fa-instagram"></i>
                         </a></li>
         </ul>
-        <div>Icons made by 
-            <a href="https://www.freepik.com" title="Freepik">Freepik</a> from 
-            <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-            <div>Icons made by 
-                <a href="https://www.flaticon.com/authors/berkahicon" title="berkahicon">berkahicon</a> from 
-                <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      
     </footer>)
  
 }

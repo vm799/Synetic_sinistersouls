86411f9e6b39d8823fa221c32b8ff4243e6b4252
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-17 15:14:09 +0100

    syneverse block resizing

--- synetic/src/components/Offer.jsx
+++ synetic/src/components/Offer.jsx
@@ -4,12 +4,13 @@ export default function Offer(){
     return(
         <div id="offer">
             <div className="pr-heading"><h1> Step into the<span>SYNEVERSE </span></h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum hic nam atque maiores modi fugiat.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Laborum hic nam atque maiores modi fugiat.</p></div>
                 <div className="pr-btns">
                     <a href="#" className="pr-btn">ENTER HERE</a>
                 </div>
            
-            </div>
+            
         </div>
     )
 }

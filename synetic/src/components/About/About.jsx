3ddca84dde936c43fdaa0967a7d2c9e55e8c479a
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-18 17:10:17 +0100

    radmap

--- synetic/src/components/About/About.jsx
+++ synetic/src/components/About/About.jsx
@@ -4,6 +4,11 @@ import "../About/About.css";
 export default function About(){
     return(
         <div className="about">
+
+        <div className="about-image">
+        <img width="700" height="400" src="https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8" alt="" />
+        </div>
+
         <div className="about-text">
             <h1>SYNETIC</h1>
             <p className="text-highlight">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus tempora, 
@@ -18,9 +23,7 @@ export default function About(){
         <button className="about-btn">CONNECT WALLET</button>
         </div>
 
-        <div className="about-image">
-        <img width="700" height="400" src="https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8" alt="" />
-        </div>
+        
         
     </div>
 

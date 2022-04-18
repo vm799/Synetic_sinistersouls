e683b3de5ad030ac9cb37aaff99b06ca95d9eb20
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-18 07:37:34 +0100

    about section

--- synetic/src/components/About/About.jsx
+++ synetic/src/components/About/About.jsx
@@ -14,6 +14,7 @@ export default function About(){
         <div className="about-image">
         <img width="700" height="400" src="https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8" alt="" />
         </div>
+        
     </div>
 
     )

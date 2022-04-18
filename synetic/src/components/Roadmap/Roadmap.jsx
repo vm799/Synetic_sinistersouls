b81d6b1a9c97e79273b5beb2b1233a4fa8c13519
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-18 17:23:38 +0100

    roadmap text box

--- synetic/src/components/Roadmap/Roadmap.jsx
+++ synetic/src/components/Roadmap/Roadmap.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Hero/Hero.css";
+import "../Roadmap/Roadmap.css";
 
 export default function Roadmap(){
     return(
@@ -21,10 +21,18 @@ export default function Roadmap(){
 
         <div className="roadmap_main-box">
         <div className="roadmap__phase">
-              <div className="roadmap__main-points"></div>
-              <div className="roadmap_text"></div>
+              <div className="roadmap__main-points">
+            Website 1.2
+            Discord community promotion
+            MINTING WAVE NFTs
+              </div>
+              <div className="roadmap_text">
+              WAVE NFTs will be 100% pre-minted and launched as an OpenOcean collection
+              </div>
         </div>
-              <div className="roadmap_image"></div>
+              <div className="roadmap_image">
+                  <img src="https://images.unsplash.com/photo-1507413245164-6160d8298b31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=270&q=80" alt=""></img>
+              </div>
               
         </div>
 

a09492294eafe59bbff627459f643995fa4a286f
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-19 19:07:44 +0100

    footer

--- synetic/src/components/Footer/Footer.jsx
+++ synetic/src/components/Footer/Footer.jsx
@@ -7,19 +7,20 @@ export default function Footer(){
         <a href="sinistersouls2922@gmail.com" class="footer__link">sinistersouls2922@gmail.com</a>
         <ul class="social-list">
             <li class="social-list__item">
-                <a class="social-list__link" href="https://uxfolio.com">
+                <a class="social-list__link" href="https://twitter.com/_sinister_souls">
                     <i class="fab fa-github"></i>
                 </a></li>
                 <li class="social-list__item">
-                    <a class="social-list__link" href="https://uxfolio.com">
+                    <a class="social-list__link" href="https://twitter.com/_sinister_souls">
                         <i class="fab fa-linkedin"></i>
                     </a></li>
                     <li class="social-list__item">
-                        <a class="social-list__link" href="https://uxfolio.com">
+                        <a class="social-list__link" href="https://twitter.com/_sinister_souls">
                             <i class="fab fa-instagram"></i>
                         </a></li>
+                        <li class="social-list__item">
                         <a href="https://www.flaticon.com/free-icons/twitter" 
-                        title="twitter icons">Twitter icons created by Md Tanvirul Haque - Flaticon</a>
+                        title="twitter icons">Twitter icons created by Md Tanvirul Haque - Flaticon</a></li>
         </ul>
       
     </footer>)

7b81917879b1afab69793dc123fd02bb30fcb002
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-02 15:39:22 +0100

    css slider

--- synetic/src/components/ImageSlider/ImageSlider.jsx
+++ synetic/src/components/ImageSlider/ImageSlider.jsx
@@ -6,11 +6,16 @@ export default function ImageSlider(){
 <div class="image-slider">
     <div class="marquee">
   <ul class="marquee-content">
-   <li>1</li>
-   <li>2</li>
-   <li>3</li>
-   <li>4</li>
-   <li>5</li>
+   <li>SYNETIC   </li>
+   <li>  
+       <img 
+   src="https://pngset.com/images/black-and-white-cow-pictures-cow-skull-clipart-gray-world-of-warcraft-transparent-png-1416545.png" alt="cow skull">
+       </img>   
+       </li>
+   <li>SINISTER SOULS</li>
+   
+  
+   
   </ul>
   </div>
 </div>

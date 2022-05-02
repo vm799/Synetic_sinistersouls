7ead26635eccd741562c56fd96f44952ddbe52ad
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-02 17:48:54 +0100

    changing class to include marquee_image

--- synetic/src/components/ImageSlider/ImageSlider.jsx
+++ synetic/src/components/ImageSlider/ImageSlider.jsx
@@ -5,23 +5,16 @@ export default function ImageSlider(){
     return(
 <div class="image-slider">
     <div class="marquee">
-  <ul class="marquee-content">
-   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
-   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
-   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
-   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
-   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
-   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
-
-  
-   
-   
-  
-   
-  </ul>
+  <div class="marquee-content">
+   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
+   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
+   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
+   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
+   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
+   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
   </div>
 </div>
-
+</div>
 
     )
 }

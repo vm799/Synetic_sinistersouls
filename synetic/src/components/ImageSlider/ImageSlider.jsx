5d8863bb7fd574692877e03a94a2c0df9ffd751c
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-02 17:35:52 +0100

    specific image sizing

--- synetic/src/components/ImageSlider/ImageSlider.jsx
+++ synetic/src/components/ImageSlider/ImageSlider.jsx
@@ -6,10 +6,13 @@ export default function ImageSlider(){
 <div class="image-slider">
     <div class="marquee">
   <ul class="marquee-content">
-   <li><img src="./images/allet" alt="alleyway"></img>  </li>
-   <li><img src="./images/allet" alt="alleyway"></img>  </li>
-   <li><img src="./images/allet" alt="alleyway"></img>  </li>
-   <li><img src="./images/allet" alt="alleyway"></img>  </li>
+   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
+   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
+   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
+   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
+   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
+   <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>  </li>
+
   
    
    

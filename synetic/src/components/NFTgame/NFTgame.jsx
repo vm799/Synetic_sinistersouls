b527e79a1b3e298ecca29a3245e7596c854c6dcb
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-02 17:20:50 +0100

    cleaning up naming

---
+++ synetic/src/components/NFTgame/NFTgame.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../NFTgame/NFTgame.css"
+
+export default function NFTgame (){
+    return(
+        <div id="main">
+          
+            <div className="NFTgame-heading">
+               <h2>NFT GAMING</h2>
+             <h1 className="NFTgame-main">SYNETIC<span> sinister</span>souls</h1>
+              
+             <p className="details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, saepe?</p>
+             <p className="details"> Lorem ipsum dolor sit amet.</p>
+             <div className="NFTgame-btn_section">
+                 <a href="mailto:sinistersouls2922@gmail.com" className="NFTgame-btn">CONTACT US</a>
+             </div>
+                </div></div>
+      
+    )
+}

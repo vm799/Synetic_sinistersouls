3d0ba301640c40f8d05513e4497a901b84a20f56
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-02 14:55:40 +0100

    slider markup

--- synetic/src/components/Feature/Feature.jsx
+++ synetic/src/components/Feature/Feature.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 import FeatureBox from "../FeatureBox/FeatureBox"
-// import fimage1 from ".../images/1.png"
-import fimage1 from "../../images/1.png"
-
-import fimage2 from "../../images/2.png"
-
-import fimage3 from "../../images/3.png"
 
 export default function Feature(){
     return(
         <div id="features">
             <h1>Features</h1>
             <div className="feature-container">
-                {/* <FeatureBox image={fimage1} title="NFTS" />
-                <FeatureBox image={fimage2} title="GAMING" />
-                <FeatureBox image={fimage3} title="SYNEVERSE" /> */}
+            
                 <FeatureBox title="NFTS" />
                 <FeatureBox title="GAMING" />
                 <FeatureBox title="SYNEVERSE" />
-                {/* <FeatureBox image={fimage4} title="Flexibility" /> */}
+        
 
                 </div>
         </div>
--- synetic/src/components/ImageSlider/ImageSlider.jsx
+++ synetic/src/components/ImageSlider/ImageSlider.jsx
@@ -3,25 +3,16 @@ import "../ImageSlider/ImageSlider.css";
 
 export default function ImageSlider(){
     return(
-        <div class="stage">
-  
-  
-<div class="container">
-  <div class="ring">
-    <div class="img"></div>
-    <div class="img"></div>
-    <div class="img"></div>
-    <div class="img"></div>
-    <div class="img"></div>
-    <div class="img"></div>
-    <div class="img"></div>
-    <div class="img"></div>
-    <div class="img"></div>
-    <div class="img"></div>
-  </div>
+<div class="marquee ">
+  <ul class="marquee-content">
+   <li>1</li>
+   <li>2</li>
+   <li>3</li>
+   <li>4</li>
+   <li>5</li>
+  </ul>
 </div>
 
-</div>
 
     )
 }

7e624d1c5ae04ccaa91087ba026a8509ac4988f3
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-02 17:53:21 +0100

    more images within slider

--- synetic/src/components/ImageSlider/ImageSlider.jsx
+++ synetic/src/components/ImageSlider/ImageSlider.jsx
@@ -4,8 +4,8 @@ import "../ImageSlider/ImageSlider.css";
 export default function ImageSlider(){
     return(
 <div class="image-slider">
-    <div class="marquee">
-  <div class="marquee-content">
+    <div class="marquee_image">
+  <div class="marquee_image-content">
    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>

9c7ed95e121fd6af5ca27aa5baa73766e4c291e6
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-03 13:30:51 +0100

    team text styles

--- synetic/src/components/TeamBox/TeamBox.jsx
+++ synetic/src/components/TeamBox/TeamBox.jsx
@@ -7,8 +7,8 @@ export default function TeamBox(props){
             <div className="team__box-img">
                 {/* <img src={props.image} alt=""/> */}
             </div>
-            <div className="team__text">
-                <p>{props.text}</p></div>
+            <div className="team__title">
+                <p>{props.title}</p></div>
             <div className="team__text">
                 <p>{props.text}</p>
         </div>

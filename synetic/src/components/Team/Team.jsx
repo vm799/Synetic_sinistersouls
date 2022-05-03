55ea0752fbf4858f7a45b59ea4617f1a1cb38937
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-03 14:26:34 +0100

    hero text

--- synetic/src/components/Team/Team.jsx
+++ synetic/src/components/Team/Team.jsx
@@ -5,21 +5,20 @@ import "../Team/Team.css"
 export default function Team(){
     return(
         <div id="team">
+            <div className="text-body">
             <h1>Meet The Team</h1>
             <h2>Created by 2 friends with common vision of disrupting reality.</h2>
-             <p> A virtual escape from the harsh confines and constraints of
-                 modern day reality on an ingenious revolutionary new digital platform. 
-                </p>
+             
 
                 <div className="straplines">
                 <p> Join Us. </p>
                 <p> Let's disrupt together.</p> 
                 </div>
-
+            </div>
             <div className="team-container">
                 <TeamBox title="Creator Lex0" text="Into retro gaming,sushi and racing dolphins"/>
                 <TeamBox title="Creator Ruyu" text="American Muscle car mad and racing snails"/>
-                <TeamBox title="Builder Panthera" text="Fav character: Yoshimitsu, deconstructed apple pies and demon dj" />
+                <TeamBox title="Builder Panthera" text="Fav character: Yoshimitsu, deconstructed apple pies" />
         
 
                 </div>

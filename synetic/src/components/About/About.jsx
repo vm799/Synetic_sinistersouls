87c0ab0f5e403723a780017f207fed6ec640a3fa
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-03 15:20:22 +0100

    highlight

--- synetic/src/components/About/About.jsx
+++ synetic/src/components/About/About.jsx
@@ -6,16 +6,16 @@ export default function About(){
         <div className="about">
       
         <div className="about-image">
-        <h1>SYNETIC</h1>
+        <h1>ABOUT SYNETIC</h1>
         <img width="700" height="400" src="https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8" alt="" />
         </div>
 
         <div className="about-text">
-            <p>Built on the blockchain.</p>
+            
             <p className="highlight-text"> A new open world multi-planetary system created with endless possibilities for escape. </p>
             <p>Live a second digital reality where nothing is off limits. </p>
-             
-              <p> Use your Sinister Soul NFTs as playable characters</p>
+            <p>Built on the blockchain.</p>
+            <p> Use your Sinister Soul NFTs as playable characters</p>
             <p className="highlight-text">EXPLORE. BUILD. OWN. EVOLVE.</p>
        
         <button className="about-btn">CONNECT WALLET</button>

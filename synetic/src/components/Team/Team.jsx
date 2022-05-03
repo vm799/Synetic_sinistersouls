7063b15fdc09615a55ca2ce7f1b11e735c4c81d5
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-03 13:42:31 +0100

    about text alignment

--- synetic/src/components/Team/Team.jsx
+++ synetic/src/components/Team/Team.jsx
@@ -9,10 +9,14 @@ export default function Team(){
             <h2>Created by 2 friends with common vision of disrupting reality.</h2>
              <p> A virtual escape from the harsh confines and constraints of
                  modern day reality on an ingenious revolutionary new digital platform. 
-                 Join Us. </p>
-                 <p> Let's disrupt together.</p> 
+                </p>
+
+                <div className="straplines">
+                <p> Join Us. </p>
+                <p> Let's disrupt together.</p> 
+                </div>
+                
             <div className="team-container">
-            
                 <TeamBox title="Creator Lex0" text="Into retro gaming and sushi"/>
                 <TeamBox title="Creator Ruyu" text="American Muscle car mad and master of my own ship"/>
                 <TeamBox title="Builder Panthera" text="Fav character: Yoshimitsu and deconstructed apple pies" />

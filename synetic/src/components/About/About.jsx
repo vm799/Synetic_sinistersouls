adaebf5b701ef8a20ccc454d55669474a57fb9ab
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-06 12:33:58 +0100

    about image size

--- synetic/src/components/About/About.jsx
+++ synetic/src/components/About/About.jsx
@@ -6,8 +6,8 @@ export default function About(){
         <div className="about">
       
         <div className="about-image">
-        <h1>ABOUT SYNETIC</h1>
-        <img width="700" height="400" src="https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8" alt="" />
+        <h1>ABOUT</h1>
+        <img width="600" height="400" src="https://images.unsplash.com/photo-1519608487953-e999c86e7455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8" alt="" />
         </div>
 
         <div className="about-text">

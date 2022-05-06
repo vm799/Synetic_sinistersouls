ce4302abae74aebc5b6bc7ce9526fa6ab6eab02b
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-06 14:35:05 +0100

    about text

--- synetic/src/components/About/About.jsx
+++ synetic/src/components/About/About.jsx
@@ -4,9 +4,9 @@ import "../About/About.css";
 export default function About(){
     return(
         <div className="about">
-        
+         <h1>ABOUT</h1>
         <div className="about-image">
-        <h1>ABOUT</h1>
+       
         <img width="600" height="400" src="https://images.unsplash.com/photo-1573767291321-c0af2eaf5266?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60" alt="" />
         </div>
 

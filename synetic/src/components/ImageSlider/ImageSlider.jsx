bb973a5b8fdc69cfecf97ffc17fa4f0ee67fd986
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-09 13:47:04 +0100

    new nft images

--- synetic/src/components/ImageSlider/ImageSlider.jsx
+++ synetic/src/components/ImageSlider/ImageSlider.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../ImageSlider/ImageSlider.css";
-import NFTimage from "../images/10.png";
+import NFTimage from "../../images/10.png"
 
 
 export default function ImageSlider(){
@@ -8,17 +8,19 @@ export default function ImageSlider(){
 <div class="image-slider">
     <div class="marquee_image">
   <div class="marquee_image-content">
-  <img src= { NFTimage } alt="synetic_souls nft"></img>
-  <img src= "../images/10.png" alt="synetic_souls nft"></img>
- 
+  <img src= { NFTimage } alt="new-synetic_souls nft"></img>
+  <img src= { NFTimage } alt="new-synetic_souls nft"></img>
+  <img src= { NFTimage } alt="new-synetic_souls nft"></img>
+  <img src= { NFTimage } alt="new-synetic_souls nft"></img>
+  <img src= { NFTimage } alt="new-synetic_souls nft"></img>
+  <img src= { NFTimage } alt="new-synetic_souls nft"></img>
+  <img src= { NFTimage } alt="new-synetic_souls nft"></img>
+  <img src= { NFTimage } alt="new-synetic_souls nft"></img>
+  <img src= { NFTimage } alt="new-synetic_souls nft"></img>
 
-   {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
-   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
-   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
-   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
-   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
-   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
-   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img> */}
+
+
+
   </div>
 </div>
 </div>

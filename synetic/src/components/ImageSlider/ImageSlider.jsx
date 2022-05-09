d2a75b9e798c0bdb4047ec7aac48f0e832bc140c
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-09 10:47:16 +0100

    new img uploaded

--- synetic/src/components/ImageSlider/ImageSlider.jsx
+++ synetic/src/components/ImageSlider/ImageSlider.jsx
@@ -6,14 +6,14 @@ export default function ImageSlider(){
 <div class="image-slider">
     <div class="marquee_image">
   <div class="marquee_image-content">
+   <img src="../../10.png" alt="synetic_souls nft"></img>
+   {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
-   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
-   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
-   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
+   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img> */}
   </div>
 </div>
 </div>

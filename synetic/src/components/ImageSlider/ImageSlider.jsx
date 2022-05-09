6a0e0d10f56d645a9eb1074635977321f2910b92
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-09 11:21:17 +0100

    move image

--- synetic/src/components/ImageSlider/ImageSlider.jsx
+++ synetic/src/components/ImageSlider/ImageSlider.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import "../ImageSlider/ImageSlider.css";
+import  "../ImageSlider/10.png";
+
 
 export default function ImageSlider(){
     return(
 <div class="image-slider">
     <div class="marquee_image">
   <div class="marquee_image-content">
-  <img src="../10.png" alt="synetic_souls nft"></img>
-  <img src="././10.png" alt="synetic_souls nft"></img>
-
-  <img src="../../10.png" alt="synetic_souls nft"></img>
-
-  <img src="src/images/10.png" alt="synetic_souls nft"></img>
+  <img src= "./10.png" alt="synetic_souls nft"></img>
+  
 
    {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQToS2ugj5oGOsjeFxTUkf6_brVUm0mgf6qQ&usqp=CAU" alt="alleyway"></img>
